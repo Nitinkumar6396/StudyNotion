1a334e8b53a17c40c76de10ee28d01e2f1d5061f
Nitin Kumar <ng6397100896@gmail.com>
2025-10-03 12:13:35 +0530

    update auth.js

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -235,6 +235,8 @@ export const login = async (req, res) => {
     const options = {
       expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
       httpOnly: true,
+      secure:true,
+      sameSite:'none'
     };
 
     return res.cookie("token", token, options).status(200).json({
